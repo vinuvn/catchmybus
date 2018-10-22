f51c1b52912a616204c3063d32518afa0a7c5a76
vinuvn <vinuvn57@gmail.com>
2018-10-22 23:41:49 +0530

    insertion

---
+++ app/src/main/java/com/example/vinu/fire/Userinformation.java
@@ -0,0 +1,67 @@
+package com.example.vinu.fire;
+
+/**
+ * Created by vinu on 10/21/2018.
+ */
+
+public class Userinformation {
+
+public String no;
+    public String r_id;
+    public String start;
+    public String stop;
+    public String time;
+    public String type;
+
+    public Userinformation() {
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getR_id() {
+        return r_id;
+    }
+
+    public void setR_id(String r_id) {
+        this.r_id = r_id;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getStop() {
+        return stop;
+    }
+
+    public void setStop(String stop) {
+        this.stop = stop;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
+
---
+++ app/src/main/java/com/example/vinu/fire/Viewdata.java
@@ -0,0 +1,94 @@
+package com.example.vinu.fire;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by vinu on 10/21/2018.
+ */
+
+public class Viewdata  extends AppCompatActivity{
+    public FirebaseDatabase mfirebase;
+    private DatabaseReference mdatabase;
+    public   String userid;
+    private ListView list1;
+    public Button read;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewdata_layout);
+        list1=(ListView)findViewById( R.id.listdata);
+        mfirebase=FirebaseDatabase.getInstance();
+        mdatabase=mfirebase.getReference("bus");
+        read=(Button)findViewById(R.id.button5);
+        Toast.makeText(getApplicationContext(),"datasnap", Toast.LENGTH_SHORT).show();
+        read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mdatabase.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Toast.makeText(getApplicationContext(),"datachange", Toast.LENGTH_SHORT).show();
+                        showdata(dataSnapshot);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        });
+
+
+
+
+
+    }
+
+    public void showdata(DataSnapshot dataSnapshot) {
+        Toast.makeText(getApplicationContext(),"datachange1111", Toast.LENGTH_SHORT).show();
+        ArrayList<String> array=new ArrayList<>();
+        for (DataSnapshot ds:dataSnapshot.getChildren())
+        {
+           Userinformation user=new Userinformation();
+            userid=ds.getKey().toString();
+            Toast.makeText(getApplicationContext(),userid, Toast.LENGTH_SHORT).show();
+           // Toast.makeText(getApplicationContext(),userid, Toast.LENGTH_SHORT).show();
+            String a1=  ds.child(userid).child("start").getValue(String.class);
+            Toast.makeText(getApplicationContext(),a1, Toast.LENGTH_SHORT).show();
+          //**  user.setNo(ds.child(userid).getValue(com.example.vinu.fire.Userinformation.class).getNo());
+          //**  user.setStart(ds.child(userid).getValue(com.example.vinu.fire.Userinformation.class).getStart());
+           // Log.d(TAG,"name: "+user.getName());
+            //Log.d(TAG,"name: "+user.getClasses());
+            //Log.d()
+           // ArrayList<String> array=new ArrayList<>();
+            array.add(a1);
+           // array.add(user.getNo());
+          //  array.add(user.getStart());
+
+        }
+        ArrayAdapter adpter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,array);
+        list1.setAdapter(adpter);
+    }
+}
+
