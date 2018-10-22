ef5be56316f3971d45ac6abff7d51ba3ca7ffa26
vishnudevs <teamboyzvishnudev@gmail.com>
2018-10-23 00:33:01 +0530

    Update app/src/main/java/com/example/vinu/fire/Test.java
    new class
    
    Co-Authored-By: vishnudevs <vishnudevs@users.noreply.github.com>

---
+++ app/src/main/java/com/example/vinu/fire/Test.java
@@ -0,0 +1,42 @@
+package com.example.vinu.fire;
+
+/**
+ * Created by vinu on 10/23/2018.
+ */
+
+public class Test {
+
+    private  String f1;
+    private  String f2;
+    private  String f3;
+
+    public Test(String f1, String f2, String f3) {
+        this.f1 = f1;
+        this.f2 = f2;
+        this.f3 = f3;
+    }
+
+    public String getF1() {
+        return f1;
+    }
+
+    public void setF1(String f1) {
+        this.f1 = f1;
+    }
+
+    public String getF2() {
+        return f2;
+    }
+
+    public void setF2(String f2) {
+        this.f2 = f2;
+    }
+
+    public String getF3() {
+        return f3;
+    }
+
+    public void setF3(String f3) {
+        this.f3 = f3;
+    }
+}
