126684ce51a4f516e5c1ca3e5bda8c8af46e49eb
vinuvn <vinuvn57@gmail.com>
2018-10-27 00:13:00 +0530

    reading data with time and type continued

--- app/src/main/java/com/example/vinu/fire/MainActivity.java
+++ app/src/main/java/com/example/vinu/fire/MainActivity.java
@@ -21,7 +21,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-//import java.util.HashMap;
+//import java.util.HashMap;..
 
 import java.util.Map;
 
